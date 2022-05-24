1af569e64f0ff200905f0848a054a0d7363ec1d5
Aibar <55969333+iBar9522@users.noreply.github.com>
2022-05-24 12:31:20 +0600

    Add purge function

--- Sources/RemoteImageView/RemoteImageFetcher.swift
+++ Sources/RemoteImageView/RemoteImageFetcher.swift
@@ -53,4 +53,8 @@ public class RemoteImageFetcher: ObservableObject {
     public func getUrl() -> URL {
         return url
     }
+    
+    public func purge() {
+        imageData = Data()
+    }
 }
